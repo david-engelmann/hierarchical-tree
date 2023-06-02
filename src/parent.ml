5467dce472c368ade47ed01e02de5c92e9faf876
David Engelmann <david+4113@satisfilabs.com>
2023-06-02 12:53:25 -0400

    create function for printing int trees

--- src/parent.ml
+++ src/parent.ml
@@ -5,9 +5,23 @@ open Async
 
 type 'a tree =
   | Leaf
-  | Node of { value : 'a; parent : 'a tree option; children : 'a tree list; level: int }
+  | Node of {
+      value : 'a;
+      parent : 'a tree option;
+      children : 'a tree list;
+      layer : int;
+    }
 
-let tree4 : int tree = Node { value = 4; parent = None; children = []; level = 0 }
+let rec print_int_tree (t : int tree) : unit =
+  match t with
+  | Leaf -> ()
+  | Node { value; children; layer; _ } ->
+      let indentation = String.make (layer * 2) ' ' in
+      print_endline (indentation ^ "Value: " ^ string_of_int value);
+      List.iter print_int_tree children
+
+let tree4 : int tree =
+  Node { value = 4; parent = None; children = []; layer = 0 }
 
 let parent_tree : int tree =
   Node
@@ -19,12 +33,14 @@ let parent_tree : int tree =
           Node
             {
               value = 1;
-              parent = Some (Node { value = 0; parent = None; children = []; level = 0 });
+              parent =
+                Some
+                  (Node { value = 0; parent = None; children = []; layer = 0 });
               children = [];
-              level = 1;
+              layer = 1;
             };
         ];
-      level = 0;
+      layer = 0;
     }
 
 let grand_parent_tree : int tree =
@@ -37,20 +53,29 @@ let grand_parent_tree : int tree =
           Node
             {
               value = 1;
-              parent = Some (Node { value = 0; parent = None; children = []; level = 0 });
+              parent =
+                Some
+                  (Node { value = 0; parent = None; children = []; layer = 0 });
               children =
                 [
                   Node
                     {
                       value = 2;
                       parent =
-                          Some (Node { value = 1; parent = None; children = []; level = 1});
+                        Some
+                          (Node
+                             {
+                               value = 1;
+                               parent = None;
+                               children = [];
+                               layer = 1;
+                             });
                       children = [];
-                      level = 2;
+                      layer = 2;
                     };
                 ];
-              level = 1
+              layer = 1;
             };
         ];
-      level = 0;
+      layer = 0;
     }
