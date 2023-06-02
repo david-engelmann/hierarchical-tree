473033c5fd632f5598f7b4123df7ebaa0046f597
David Engelmann <david+4113@satisfilabs.com>
2023-06-02 09:25:45 -0400

    example tree, parent and grand parent

--- src/parent.ml
+++ src/parent.ml
@@ -3,24 +3,49 @@ open Core
 open Async
 *)
 
+type 'a tree =
+  | Leaf
+  | Node of { value : 'a; parent : 'a tree option; children : 'a tree list }
 
-type 'a node =
-    | Leaf
-    | Node of { value: 'a; parent: 'a node option; children: 'a node list }
+let tree4 : int tree = Node { value = 4; parent = None; children = [] }
 
+let parent_tree : int tree =
+  Node
+    {
+      value = 0;
+      parent = None;
+      children =
+        [
+          Node
+            {
+              value = 1;
+              parent = Some (Node { value = 0; parent = None; children = [] });
+              children = [];
+            };
+        ];
+    }
 
-(*{
-    name: string;
-    parent: string;
-    children:
-}*)
-
-(*let son_node: node = {
-    name = "son";
-    parent = "dad";
-}
-
-let parent_node: node = {
-    name = "dad";
-    parent = "gp";
-}*)
+let grand_parent_tree : int tree =
+  Node
+    {
+      value = 0;
+      parent = None;
+      children =
+        [
+          Node
+            {
+              value = 1;
+              parent = Some (Node { value = 0; parent = None; children = [] });
+              children =
+                [
+                  Node
+                    {
+                      value = 2;
+                      parent =
+                        Some (Node { value = 1; parent = None; children = [] });
+                      children = [];
+                    };
+                ];
+            };
+        ];
+    }
