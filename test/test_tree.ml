8dd62fa2d33788a89639be146530a6e68ac1909d
David Engelmann <david+4113@satisfilabs.com>
2023-06-05 16:14:49 -0400

    add test for get_lowest_layer_of_tree

--- test/test_tree.ml
+++ test/test_tree.ml
@@ -1,19 +1,20 @@
-(*
 open Core
-open Async
 open OUnit2
-open Tree
-*)
+open Hierarchical_tree.Tree
 
 let add x y = x + y
 
 let test_addition _ =
-  assert_equal 5 (add 3 3)
+  OUnit2.assert_equal 5 (add 2 3)
+
+let test_get_lowest_layer_of_tree_on_tree4 _ =
+  OUnit2.assert_equal 1 (Tree.get_lowest_layer_of_tree Tree.tree4)
 
 let suite =
     "suite" >:::
         [
             "test_addition" >:: test_addition;
+            "test_get_lowest_layer_of_tree_on_tree4" >:: test_get_lowest_layer_of_tree_on_tree4;
         ]
 
 let () =
