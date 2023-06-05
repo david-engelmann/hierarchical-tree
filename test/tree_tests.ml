5c90cafac2af86a495ccafcb023f947b11b111cb
David Engelmann <david+4113@satisfilabs.com>
2023-06-05 12:06:21 -0400

    add basic test flow

--- test/tree_tests.ml
+++ test/tree_tests.ml
@@ -4,5 +4,16 @@ open Async
 open OUnit2
 *)
 
+let add x y = x + y
 
+let test_addition _ =
+  assert_equal 5 (add 2 3)
 
+let suite =
+    "suite" >:::
+        [
+            "test_addition" >:: test_addition;
+        ]
+
+let () =
+  run_test_tt_main suite
