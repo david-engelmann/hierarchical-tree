2a0cfb0779c38820a09714f958c3430c7170c32f
David Engelmann <81196676+david-engelmann@users.noreply.github.com>
2023-06-05 21:41:56 -0400

    remove addition test

--- test/test_tree.ml
+++ test/test_tree.ml
@@ -2,11 +2,6 @@ open Core
 open OUnit2
 open Hierarchical_tree.Tree
 
-let add x y = x + y
-
-let test_addition _ =
-  OUnit2.assert_equal 5 (add 2 3)
-
 let test_get_lowest_layer_of_tree_on_tree4 _ =
   OUnit2.assert_equal 1 (Tree.get_lowest_layer_of_tree Tree.tree4)
 
@@ -19,7 +14,6 @@ let test_get_lowest_layer_of_tree_on_grand_parent_tree _ =
 let suite =
     "suite" >:::
         [
-            "test_addition" >:: test_addition;
             "test_get_lowest_layer_of_tree_on_tree4" >:: test_get_lowest_layer_of_tree_on_tree4;
             "test_get_lowest_layer_of_tree_on_parent_tree" >:: test_get_lowest_layer_of_tree_on_parent_tree;
 "test_get_lowest_layer_of_tree_on_grand_parent_tree" >:: test_get_lowest_layer_of_tree_on_grand_parent_tree;
