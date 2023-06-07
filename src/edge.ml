fca16343b38253cd24949cc99346b76d5d0e078f
David Engelmann <david+4113@satisfilabs.com>
2023-06-07 17:47:04 -0400

    first attempt at to_string for edges

--- src/edge.ml
+++ src/edge.ml
@@ -6,6 +6,14 @@ module Edge = struct
     edge_data : ('a * 'b) list;
   }
 
+  let to_string_int_string (t : (int, string) edge) : string =
+    let edge_data_str = List.map (fun (x, y) -> "(" ^ string_of_int x ^ ", " ^ "\"" ^ y ^ "\"" ^ ")") t.edge_data in
+    let edge_data_str = String.concat "; " edge_data_str in
+    "{ source_node_id = " ^ string_of_int t.source_node_id ^
+    "; target_node_id = " ^ string_of_int t.target_node_id ^
+    "; weight = " ^ string_of_int t.weight ^
+    "; edge_data = [" ^ edge_data_str ^ "] }"
+
   let edge_int_string : (int, string) edge =
     {
       source_node_id = 0;
