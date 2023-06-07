e80f8687b47b54e40204458a25d9888357efd72d
David Engelmann <david+4113@satisfilabs.com>
2023-06-07 16:49:27 -0400

    should be Edge module

--- test/test_edge.ml
+++ test/test_edge.ml
@@ -3,62 +3,62 @@ open OUnit2
 open Hierarchical_tree.Edge
 
 let test_edge_int_string_source_node_id _ =
-  match Node.edge_int_string with
+  match Edge.edge_int_string with
    | { source_node_id; _ } ->
         OUnit2.assert_equal 0 source_node_id
 
 let test_edge_int_string_target_node_id _ =
-  match Node.edge_int_string with
+  match Edge.edge_int_string with
    | { target_node_id; _ } ->
         OUnit2.assert_equal 1 target_node_id
 
 let test_edge_int_string_weight _ =
-  match Node.edge_int_string with
+  match Edge.edge_int_string with
    | { weight; _ } ->
         OUnit2.assert_equal 1 weight
 
 let test_edge_string_string_source_node_id _ =
-  match Node.edge_string_string with
+  match Edge.edge_string_string with
    | { source_node_id; _ } ->
         OUnit2.assert_equal 0 source_node_id
 
 let test_edge_string_string_target_node_id _ =
-  match Node.edge_string_string with
+  match Edge.edge_string_string with
    | { target_node_id; _ } ->
         OUnit2.assert_equal 1 target_node_id
 
 let test_edge_string_string_weight _ =
-  match Node.edge_string_string with
+  match Edge.edge_string_string with
    | { weight; _ } ->
         OUnit2.assert_equal 1 weight
 
 let test_edge_string_int_source_node_id _ =
-  match Node.edge_string_int with
+  match Edge.edge_string_int with
    | { source_node_id; _ } ->
         OUnit2.assert_equal 0 source_node_id
 
 let test_edge_string_int_target_node_id _ =
-  match Node.edge_string_int with
+  match Edge.edge_string_int with
    | { target_node_id; _ } ->
         OUnit2.assert_equal 1 target_node_id
 
 let test_edge_string_int_weight _ =
-  match Node.edge_string_int with
+  match Edge.edge_string_int with
    | { weight; _ } ->
         OUnit2.assert_equal 1 weight
 
 let test_edge_int_int_source_node_id _ =
-  match Node.edge_int_int with
+  match Edge.edge_int_int with
    | { source_node_id; _ } ->
         OUnit2.assert_equal 0 source_node_id
 
 let test_edge_int_int_target_node_id _ =
-  match Node.edge_int_int with
+  match Edge.edge_int_int with
    | { target_node_id; _ } ->
         OUnit2.assert_equal 1 target_node_id
 
 let test_edge_int_int_weight _ =
-  match Node.edge_int_int with
+  match Edge.edge_int_int with
    | { weight; _ } ->
         OUnit2.assert_equal 1 weight
 
