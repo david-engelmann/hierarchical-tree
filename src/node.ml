cabdcc1c100f5bdf2130cec4938487e3a71d81c7
David Engelmann <david+4113@satisfilabs.com>
2023-06-07 21:55:04 -0400

    to_string for int_int_node

--- src/node.ml
+++ src/node.ml
@@ -26,6 +26,14 @@ module Node = struct
       "; node_name = " ^ n.node_name ^
       "; node_data = [" ^ node_data_str ^ "] }"
 
+    let to_string_int_int_node ( n : (int, int) node) : string =
+      let node_data_str = List.map (fun (x, y) -> "(" ^ "\"" ^ string_of_int x ^ "\"" ^ ", " ^ "\"" ^ string_of_int y ^ "\"" ^ ")") n.node_data in
+      let node_data_str = String.concat "; " node_data_str in
+      "{ node_id = " ^ string_of_int n.node_id ^
+      "; node_name = " ^ n.node_name ^
+      "; node_data = [" ^ node_data_str ^ "] }"
+
+
     let node_int_string : (int, string) node = { node_id = 0; node_name = "july_jackson"; node_data = [(0, "wife"); (1, "kids")] }
 
     let node_string_string : (string, string) node = { node_id = 1; node_name = "tom_turkey"; node_data = [("low", "down"); ("your", "friend")] }
